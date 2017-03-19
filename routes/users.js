1a455090d0f97f8250dada9e367cb2ad20ec21fc
EDA <eda@mac-mini-3.local>
2017-03-20 11:45:21 +1300

    can get list of names

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 var db = require('../db')
 
-router.get ('/', function (req, res) {
+router.get('/', function (req, res) {
   db.getUsers(req.app.get('knex'))
     .then(function (users) {
       res.send({ users: users })
@@ -12,5 +12,20 @@ router.get ('/', function (req, res) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
 })
+// router.get('/:id', funtion (req, res) {
+//
+// })
+
+router.get('/usernames', function (req, res) {
+  db.getUsers(req.app.get('knex'))
+    .then(getUserNames)
+    .then(function (users) {
+      res.send(users)
+    })
+    .catch(function (err) {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+const getUserNames = (users) => (users.map((user) => user.name))
 
 module.exports = router
